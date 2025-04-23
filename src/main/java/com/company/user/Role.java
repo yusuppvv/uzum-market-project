1e01d79d6073b0a8e414bce417da58d25f5f0774
Azizbek <azizbeky250@gmail.com>
2025-04-23 14:58:58 +0500

    Changed entities

--- src/main/java/com/company/user/Role.java
+++
@@ -1,8 +0,0 @@
-package com.company.user;
-
-public enum Role {
-    USER,
-    SELLER,
-    ADMIN,
-    MODERATOR
-}
--- src/main/java/com/company/user/Status.java
+++
@@ -1,7 +0,0 @@
-package com.company.user;
-
-public enum Status {
-    ACTIVE,
-    INACTIVE,
-    BANNED
-}
