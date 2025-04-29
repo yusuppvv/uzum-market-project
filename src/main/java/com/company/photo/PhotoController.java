78bb9ab2c487b437a995aed31d621b0a91f02173
Azizbek <azizbeky250@gmail.com>
2025-04-29 13:35:52 +0500

    Half of photo crud

--- src/main/java/com/company/photo/PhotoController.java
+++ src/main/java/com/company/photo/PhotoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -25,6 +26,26 @@ public class PhotoController {
         );
     }
 
+    @PostMapping("/multi-upload")
+    public ResponseEntity<List<PhotoResp>> multiUpload(@RequestParam("files") MultipartFile [] files,
+                                                       @RequestParam UUID productId) {
+        return ResponseEntity.ok(
+                photoService.multiUpload(files, productId)
+        );
+
+    }
+
+    @GetMapping("/metadata/{id}")
+    public ResponseEntity<PhotoResp> metadata(@PathVariable UUID id) {
+        return ResponseEntity.ok(photoService.metadata(id));
+    }
+
+
+    //Delete by id
+    @DeleteMapping("/delete-by-id/{id}")
+    public ResponseEntity<String> delete(@PathVariable UUID id) {
+        return ResponseEntity.ok(photoService.deleteById(id));
+    }
     @GetMapping(value = "/{id}",
     produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     public ResponseEntity<byte[]> getById(@PathVariable UUID id) {
--- src/main/java/com/company/photo/PhotoService.java
+++ src/main/java/com/company/photo/PhotoService.java
@@ -7,8 +7,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -34,6 +39,23 @@ public class PhotoService {
         return toDTO(photo);
     }
 
+    public String deleteById(UUID id) {
+        Optional<PhotoEntity> byIdAndVisibilityTrue = photoRepository.findByIdAndVisibilityTrue(id);
+        if (byIdAndVisibilityTrue.isPresent()) {
+            byIdAndVisibilityTrue.get().setVisibility(false);
+            return "Successfully deleted";
+        }
+        else return "Photo not found";
+    }
+
+    public PhotoResp metadata(UUID id) {
+        Optional<PhotoEntity> byIdAndVisibilityTrue = photoRepository.findByIdAndVisibilityTrue(id);
+        if (byIdAndVisibilityTrue.isPresent()) {
+            return toDTO(byIdAndVisibilityTrue.get());
+        }
+        else return null;
+    }
+
     private PhotoResp toDTO(PhotoEntity photo) {
         return new PhotoResp(photo.getId(), photo.getName(), photo.getProductId());
     }
@@ -47,5 +69,11 @@ public class PhotoService {
                     .getData();
         }
             throw new ItemNotFoundException();
+    }
+
+    public List<PhotoResp> multiUpload(MultipartFile[] files, UUID productId) {
+        return Arrays.stream(files)
+                .map(file -> upload(file, productId))
+                .collect(toList());
     }
 }
