134b600df792c368a0cba5ea019bdc3194b63ec3
Behruz <hasanovbehruz121@gmail.com>
2025-04-30 16:15:39 +0500

    review crud

--- src/main/java/com/company/review/ReviewController.java
+++ src/main/java/com/company/review/ReviewController.java
@@ -1,12 +1,24 @@
 package com.company.review;
 
+import com.company.product.ProductEntity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<ReviewEntity> createReview(@RequestBody ReviewEntity review) {
+        return ResponseEntity.ok(reviewService.createReview(review));
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getAllReviews() {
+        return ResponseEntity.ok(reviewService.getAllReviews());
+    }
+
 }
--- src/main/java/com/company/review/ReviewService.java
+++ src/main/java/com/company/review/ReviewService.java
@@ -3,8 +3,23 @@ package com.company.review;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+
+    public ReviewEntity createReview(ReviewEntity review) {
+        ReviewEntity save = reviewRepository.save(review);
+        return save;
+    }
+
+    public String getAllReviews() {
+        List<ReviewEntity> all = reviewRepository.findAll();
+       return all.stream()
+               .map(ReviewEntity::toString)
+               .collect(Collectors.joining("\n"));
+    }
 }
