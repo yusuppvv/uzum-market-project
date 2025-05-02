99ec0f93e9465c0fd660b48e9533c3f28b67e298
Azizbek <azizbeky250@gmail.com>
2025-05-02 09:03:39 +0500

    Created cart.http

--- src/main/resources/sql/review_trigger.sql
+++ src/main/resources/sql/review_trigger.sql
@@ -35,3 +35,18 @@ CREATE OR REPLACE TRIGGER trigger_review_rating
     AFTER INSERT OR UPDATE ON review
                                   FOR EACH ROW
                                   EXECUTE FUNCTION function_review_rating();
+
+
+
+INSERT INTO orders (
+    id, created_at, updated_at, visibility, status, total_price, type, user_id
+) VALUES (
+             '550e8400-e29b-41d4-a716-446655440000',  -- id
+             '2025-05-02 10:00:00.000000',           -- created_at
+             '2025-05-02 10:05:00.000000',           -- updated_at
+             true,                                   -- visibility
+             1,                                      -- status
+             99.99,                                  -- total_price
+             2,                                      -- type
+             '6b15732e-6068-464a-8616-abd43a39873e'  -- user_id
+         );
