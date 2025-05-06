c048b83131b1aaf257bb88e4fc33e9bd9305b4ac
Azizbek <azizbeky250@gmail.com>
2025-05-06 11:01:04 +0500

    update_delivery_status_function()

--- src/main/resources/sql/order_trigger.sql
+++ src/main/resources/sql/order_trigger.sql
@@ -2,21 +2,20 @@ CREATE OR REPLACE FUNCTION update_delivery_status_function()
     RETURNS TRIGGER AS $$
 BEGIN
     IF (TG_OP = 'UPDATE') THEN
-    IF NEW.status = 'PAYED' THEN
-        INSERT INTO delivery (
-            id, created_at, updated_at, visibility, address, status, user_id
-        )
-        VALUES (
-                   gen_random_uuid(),
-                   NOW(),
-                   NOW(),
-                   true,
-                   'Tashkent, Yunusabad 12',
-                   1,
-                   NEW.user_id
-               );
-
-    END IF;
+        IF NEW.status = 'PAYED' THEN
+            INSERT INTO delivery (
+                id, created_at, updated_at, visibility, address, status, user_id
+            )
+            VALUES (
+                       gen_random_uuid(),
+                       now(),
+                       now(),
+                       true,
+                       'Tashkent, Yunusabad 12',
+                       1,
+                       NEW.user_id
+            );
+        END IF;
     END IF;
     RETURN NEW;
 END;
