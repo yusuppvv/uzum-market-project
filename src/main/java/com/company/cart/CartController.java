0e0fef8535904911cdfeec4b8069d1e0e5320b9d
Behruz <hasanovbehruz121@gmail.com>
2025-05-07 13:06:21 +0500

    mistake

--- src/main/java/com/company/cart/CartController.java
+++ src/main/java/com/company/cart/CartController.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
